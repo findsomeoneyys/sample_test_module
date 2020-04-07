26bd8d163b7f1502c9507de21f036a366be39555
ys <yunshen.yang@foxmail.com>
2020-04-08 01:26:11 +0800

    add test py

---
+++ test_server.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import subprocess
+
+def has_test_errors(fname, dbname, odoo_version, check_loaded=True):
+    """
+    Check a list of log lines for test errors.
+    Extension point to detect false positives.
+    """
+    # Rules defining checks to perform
+    # this can be
+    # - a string which will be checked in a simple substring match
+    # - a regex object that will be matched against the whole message
+    # - a callable that receives a dictionary of the form
+    #     {
+    #         'loglevel': ...,
+    #         'message': ....,
+    #     }
+    errors_ignore = [
+        'Mail delivery failed',
+        'failed sending mail',
+        ]
+    errors_report = [
+        lambda x: x['loglevel'] == 'CRITICAL',
+        'At least one test failed',
+        'no access rules, consider adding one',
+        'invalid module names, ignored',
+        ]
+    # Only check ERROR lines before 7.0
+    if odoo_version < '7.0':
+        errors_report.append(
+            lambda x: x['loglevel'] == 'ERROR')
+
+    def make_pattern_list_callable(pattern_list):
+        for i in range(len(pattern_list)):
+            if isinstance(pattern_list[i], string_types):
+                regex = re.compile(pattern_list[i])
+                pattern_list[i] = lambda x, regex=regex:\
+                    regex.search(x['message'])
+            elif hasattr(pattern_list[i], 'match'):
+                regex = pattern_list[i]
+                pattern_list[i] = lambda x, regex=regex:\
+                    regex.search(x['message'])
+
+    make_pattern_list_callable(errors_ignore)
+    make_pattern_list_callable(errors_report)
+
+    print("-"*10)
+    # Read log file removing ASCII color escapes:
+    # http://serverfault.com/questions/71285
+    color_regex = re.compile(r'\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]')
+    log_start_regex = re.compile(
+        r'^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2},\d{3} \d+ (?P<loglevel>\w+) '
+        '(?P<db>(%s)|([?])) (?P<logger>\S+): (?P<message>.*\S)\s*$' % dbname)
+    log_records = []
+    last_log_record = dict.fromkeys(log_start_regex.groupindex.keys())
+    with open(fname) as log:
+        for line in log:
+            line = color_regex.sub('', line)
+            match = log_start_regex.match(line)
+            if match:
+                last_log_record = match.groupdict()
+                log_records.append(last_log_record)
+            else:
+                last_log_record['message'] = '%s\n%s' % (
+                    last_log_record['message'], line.rstrip('\n')
+                )
+    errors = []
+    for log_record in log_records:
+        ignore = False
+        for ignore_pattern in errors_ignore:
+            if ignore_pattern(log_record):
+                ignore = True
+                break
+        if ignore:
+            continue
+        for report_pattern in errors_report:
+            if report_pattern(log_record):
+                errors.append(log_record)
+                break
+
+    if check_loaded:
+        if not [r for r in log_records if 'Modules loaded.' in r['message']]:
+            errors.append({'message': "Message not found: 'Modules loaded.'"})
+
+    if errors:
+        for e in errors:
+            print(e['message'])
+        print("-"*10)
+    return len(errors)
+
+if __name__ == '__main__':
+    pipe = subprocess.Popen("odoo --db_host postgres -d postgres --db_user odoo --db_password odoo -i base --test-enable --stop-after-init",
+                            stderr=subprocess.STDOUT,
+                            stdout=subprocess.PIPE)
+    with open('stdout.log', 'wb') as stdout:
+        for line in iter(pipe.stdout.readline, b''):
+            stdout.write(line)
+            print(line.strip().decode(
+                'UTF-8', errors='backslashreplace'
+            ))
+    returncode = pipe.wait()
+    # Find errors, except from failed mails
+    errors = has_test_errors(
+        "stdout.log", 'odoo', '12.0', False)
+    if returncode != 0:
+        all_errors.append(to_test)
+        print(fail_msg, "Command exited with code %s" % returncode)
+        # If there are no errors,
+        # adds an error when returcode!=0
+        # because it's actually an error.
+        if not errors:
+            errors += 1
+    if errors:
+        counted_errors += errors
+        all_errors.append(to_test)
+        print(fail_msg, "Found %d lines with errors" % errors)
+        return 1
+    else:
+        return 0
