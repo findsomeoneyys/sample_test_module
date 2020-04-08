aa138f53575c2312bc523f21e650d408b70b5c96
ys <yunshen.yang@foxmail.com>
2020-04-08 15:42:30 +0800

    add run

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
-# -*- coding: utf-8 -*-
-__import__('os').environ['TZ'] = 'UTC'
+#!/usr/bin/env python3
 
+# set server timezone in UTC before time module imported
+__import__('os').environ['TZ'] = 'UTC'
 import odoo
 
 if __name__ == "__main__":
